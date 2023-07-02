4eff8323a52accd4092c0a6b44e718f84c31f20b
Vishal Ekka <vishalekka18@gmail.com>
2023-07-02 17:09:00 +0530

    Small changes

--- details.js
+++ details.js
@@ -1,5 +1,6 @@
 
 // Function to fetch anime details by title
+
 function fetchAnimeDetails(title) {
   fetch(`https://api.jikan.moe/v4/anime?q=${title}`)
     .then((response) => response.json())
@@ -8,6 +9,8 @@ function fetchAnimeDetails(title) {
       const characterGalleryContainer = document.querySelector('#character-gallery');
       animeDetailsContainer.innerHTML = '';
       characterGalleryContainer.innerHTML = '';
+      
+      
 
       if (data.data.length === 0) {
         const noResultsMessage = document.createElement('p');
@@ -32,51 +35,51 @@ function fetchAnimeDetails(title) {
 
         const score = document.createElement('div');
         score.classList.add('anime-score');
-        score.textContent = `Score: ${anime.score}`;
+        score.innerHTML = `<strong>Score:</strong> ${anime.score}`;
         animeCard.appendChild(score);
 
         const episodes = document.createElement('div');
         episodes.classList.add('anime-episodes');
-        episodes.textContent = `Episodes: ${anime.episodes}`;
+        episodes.innerHTML = `<strong>Episodes:</strong> ${anime.episodes}`;
         animeCard.appendChild(episodes);
 
         if (anime.studios && anime.studios.length > 0) {
           const studios = document.createElement('div');
           studios.classList.add('anime-studios');
-          studios.textContent = `Studios: ${anime.studios.map((studio) => studio.name).join(', ')}`;
+          studios.innerHTML = `<strong>Studios:</strong> ${anime.studios.map((studio) => studio.name).join(', ')}`;
           animeCard.appendChild(studios);
         }
         
         if (anime.genres && anime.genres.length > 0) {
           const genres = document.createElement('div');
           genres.classList.add('anime-genres');
-          genres.textContent = `Genres: ${anime.genres.map((genre) => genre.name).join(', ')}`;
+          genres.innerHTML = `<strong>Genres:</strong>${anime.genres.map((genre) => genre.name).join(', ')}`;
           animeCard.appendChild(genres);
         }
         if (anime.producers && anime.producers.length > 0) {
           const producers = document.createElement('div');
           producers.classList.add('anime-producers');
-          producers.textContent = `Producers: ${anime.producers.map((producer) => producer.name).join(', ')}`;
+          producers.innerHTML = `<strong>Producers:</strong> ${anime.producers.map((producer) => producer.name).join(', ')}`;
           animeCard.appendChild(producers);
         }
 
         if (anime.source) {
           const source = document.createElement('div');
           source.classList.add('anime-source');
-          source.textContent = `Source: ${anime.source}`;
+          source.innerHTML= `<strong>Source</strong>: ${anime.source}`;
           animeCard.appendChild(source);
         }
         
         if (anime.year) {
           const year = document.createElement('div');
           year.classList.add('anime-year');
-          year.textContent = `Year of Release: ${anime.year}`;
+          year.innerHTML = `<strong>Year of Release:</strong> ${anime.year}`;
           animeCard.appendChild(year);
         }
         if (anime.synopsis) {
           const synopsis = document.createElement('div');
           synopsis.classList.add('anime-synopsis');
-          synopsis.textContent = `Synopsis: ${anime.synopsis}`;
+          synopsis.innerHTML = `<u><strong>Synopsis:<strong></u> ${anime.synopsis}`;
           animeCard.appendChild(synopsis);
         }
     
